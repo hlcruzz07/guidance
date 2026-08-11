5428cbb52163812c8ddb460a8df2ffdf8d0292be
harold <harold.cruz0407@gmail.com>
2026-08-11 12:49:42 +0800

    hahaha

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/form', [StudentController::class, 'form'])->name('studentForm');
 Route::post('/form/store', [StudentController::class, 'store'])->name('storeStudent');
 
-// Guest Routes
+// Guest Routes asdasdasd
 Route::middleware('guest')->group(function () {
     Route::inertia('/', 'welcome')->name('home');
     Route::get('/admin', [AdminController::class, 'index'])->name('admin');
